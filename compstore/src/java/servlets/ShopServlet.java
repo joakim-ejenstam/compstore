0a6c4b7418f65ac5a862c35e0aa4627d20453ede
Martin <martinbjorling@gmail.com>
2013-05-14 16:20:27 +0200

    Working servlet

--- compstore/src/java/servlets/ShopServlet.java
+++ compstore/src/java/servlets/ShopServlet.java
@@ -7,16 +7,14 @@ package servlets;
 import java.io.IOException;
 import java.io.PrintWriter;
 import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 /**
  *
- * @author Joakim
+ * @author Martin
  */
-@WebServlet(name = "ShopServlet", urlPatterns = {"/ShopServlet"})
 public class ShopServlet extends HttpServlet {
 
     /**
