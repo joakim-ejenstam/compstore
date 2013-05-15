ab2e1714490253667d373898d2c065c53478d0fa
Martin <martinbjorling@gmail.com>
2013-05-15 12:44:16 +0200

    Working servlet

---
+++ compstore/src/java/beans/ComputerBean.java
@@ -0,0 +1,55 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package beans;
+
+import java.util.*;
+import java.sql.*;
+import java.io.*;
+
+/**
+ *
+ * @author Martin
+ */
+public class ComputerBean {
+    
+    //describes a computer
+    
+    private String url=null;
+    
+    Connection conn = null;
+    Statement stmt = null;
+    ResultSet rs = null;
+    
+    private int id;
+    private String name;
+    private String description;
+    
+    public ComputerBean() {
+    }
+    
+    public int getID() {
+        return id;
+    }
+    
+    public void setID(int _id) {
+        id = _id;
+    }
+    
+    public String getName() {
+        return name;
+    }
+    
+    public void setName(String _name) {
+        name = _name;
+    }
+    
+    public String getDescription() {
+        return description;
+    }
+    
+    public void setDescription(String _description) {
+        description = _description;
+    }
+}
--- compstore/src/java/servlets/ShopServlet.java
+++ compstore/src/java/servlets/ShopServlet.java
@@ -39,7 +39,7 @@ public class ShopServlet extends HttpServlet {
             out.println("<title>Servlet ShopServlet</title>");            
             out.println("</head>");
             out.println("<body>");
-            out.println("<h1>Servlet ShopServlet at " + request.getContextPath() + "</h1>");
+            out.println("<h1>Servlet ShopServlet at (AKTA SÅ JAG INTE PULLAR DIG!!!!) " + request.getContextPath() + "</h1>");
             out.println("</body>");
             out.println("</html>");
         } finally {            
